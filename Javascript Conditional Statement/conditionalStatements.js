5e8a8f60affe7aea4a8710fba4442fdea2f8506d
Muhammad-Allyan <muhammadallyanahmad25@gmail.com>
2025-08-05 22:17:06 +0500

    10 17 PM

--- Javascript Conditional Statement/conditionalStatements.js
+++ Javascript Conditional Statement/conditionalStatements.js
@@ -7,4 +7,35 @@ if (num % 2 === 0) {
 }
 }
 let result = evenOddFunc(12);
-console.log(result);    
+console.log(result);
+
+// ✅ 2. Check Positive, Negative, or Zero
+let checkPNZNumber = 25;
+if (checkPNZNumber > 0) {
+  console.log("The number is positive!");
+}
+else if (checkPNZNumber < 0) {
+  console.log("The number is negative!");
+} else{
+  console.log("The number is Zero");
+}
+
+// ✅ 3. Check if Number is Divisible by 5 and 3
+let checkDivisibleNumber = 15;
+if (checkDivisibleNumber % 5 === 0 && checkDivisibleNumber % 3 === 0 ) {
+  console.log("The number is divisible by both 5 and 3");
+}
+else{
+  console.log("Not divisible by both");
+}
+
+// ✅ 4. Age Eligibility for Voting
+let ageEligibility = 17;
+if (ageEligibility >= 18) {
+  console.log("Eligible to vote!");
+}
+else{
+  console.log("Not eligible to vote!");
+}
+
+// ✅ 5. Grading System
